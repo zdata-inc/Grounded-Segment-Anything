b9c1ec7730513a757a1fbdfb2247ebf5413376d2
AshwinSakhare <asakhare@zdatainc.com>
2024-07-01 17:02:38 -0400

    add setup file:

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+# Package metadata
+NAME = 'grounded_sam'
+VERSION = '1.0.0'
+DESCRIPTION = 'zData implementation of Grounded Sam model'
+AUTHOR = 'Ashwin Sakhare'
+EMAIL = 'asakhare@zdatainc.com'
+URL = 'https://github.com/zdata-inc/Grounded-Segment-Anything'
+LICENSE = 'MIT'
+PYTHON_VERSION = '>=3.11.0'
+
+# Required packages
+with open('requirements.txt', 'r') as f:
+    required_packages = f.read().splitlines()
+
+setup(
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    author = AUTHOR,
+    author_email = EMAIL,
+    url = URL,
+    license = LICENSE,
+    python_requires = PYTHON_VERSION,
+    packages=find_packages(include=['segment_anything/segment_anything', 'GroundingDino/GroundingDino']),
+    install_requires = required_packages,
+    classifiers = [
+        'Development Status :: 4 - Production/Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.11',
+        'Operating System :: OS Independent'
+    ],
+)
